0cdfb8dc2dd647b3a08424f0000d3a600e04e393
voidSatisfaction <lourie@naver.com>
2018-01-13 15:23:00 +0900

    feat: add separate chaning hash

---
+++ pkg/hash/separate_chaning.go
@@ -0,0 +1,64 @@
+package hash
+
+import (
+	"container/list"
+)
+
+type HashKey interface {
+	hashCode() int
+	equals(hk interface{}) bool
+}
+
+type node struct {
+	key HashKey
+
+	val interface{}
+}
+
+const M int = 30001
+
+type SCHash struct {
+	M int
+
+	Data []*list.List
+}
+
+func NewSeparateChainingHash() *SCHash {
+	Data := make([]*list.List, M)
+	for i := 0; i < M; i++ {
+		Data[i] = list.New()
+	}
+	return &SCHash{
+		M:    0,
+		Data: Data,
+	}
+}
+
+func (sch *SCHash) Put(hk HashKey, val interface{}) {
+	i := (hk.hashCode() % M)
+
+	for e := sch.Data[i].Front(); e != nil; e = e.Next() {
+		if e.Value.(*node).key.equals(hk) {
+			e.Value.(*node).val = val
+			return
+		}
+	}
+
+	newNode := &node{
+		key: hk,
+		val: val,
+	}
+	sch.Data[i].PushFront(newNode)
+}
+
+func (sch *SCHash) Get(hk HashKey) (interface{}, bool) {
+	i := (hk.hashCode() % M)
+
+	for e := sch.Data[i].Front(); e != nil; e = e.Next() {
+		if e.Value.(*node).key.equals(hk) {
+			return e.Value.(*node).val, true
+		}
+	}
+
+	return nil, false
+}
---
+++ pkg/hash/separate_chaning_test.go
@@ -0,0 +1,84 @@
+package hash
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type stringHashKey string
+
+// hashCode should not mod M which is array size
+// because it is moded at hash implement code
+func (shk stringHashKey) hashCode() int {
+	str := string(shk)
+	var hc int
+	for i := 0; i < len(str); i++ {
+		hc = 31*hc + int(str[i])
+	}
+	return hc & (1<<31 - 1)
+}
+
+func (shk stringHashKey) equals(hk interface{}) bool {
+	hk, ok := hk.(stringHashKey)
+	if !ok {
+		panic("hk is not converted to stringHashKey type")
+	}
+	return shk == hk
+}
+
+func TestSeparateChaningHashTest(t *testing.T) {
+	// test for get success
+	successTests := []struct {
+		key       stringHashKey
+		expectOk  bool
+		expectVal interface{}
+	}{
+		{stringHashKey("abc"), true, "hello"},
+		{stringHashKey("ccccsdcasd"), true, []int{1, 2, 3}},
+		{stringHashKey("222"), true, "hihihi"},
+		{stringHashKey("wkfm4io3fj3432341231zcxv9fknalkdsfl"), true, true},
+	}
+
+	sch := NewSeparateChainingHash()
+
+	sch.Put(stringHashKey("abc"), "hello")
+	sch.Put(stringHashKey("ccccsdcasd"), []int{1, 2, 3})
+	sch.Put(stringHashKey("222"), "hihihi")
+	sch.Put(stringHashKey("wkfm4io3fj3432341231zcxv9fknalkdsfl"), true)
+
+	for _, test := range successTests {
+		key, expectOk, expectVal := test.key, test.expectOk, test.expectVal
+		fmt.Println(key.hashCode())
+		val, ok := sch.Get(key)
+		if !reflect.DeepEqual(expectVal, val) {
+			t.Errorf("value is not as expected")
+			t.Errorf("expectVal: %+v, got: %+v", expectVal, val)
+		}
+
+		if ok != expectOk {
+			t.Errorf("ok is not as expected")
+			t.Errorf("expectOk: %+v, got: %+v", expectOk, ok)
+		}
+	}
+
+	// test for get fail
+	failTests := []struct {
+		key      stringHashKey
+		expectOk bool
+	}{
+		{stringHashKey("abc11"), false},
+		{stringHashKey("ccccsdcasasdfadd"), false},
+		{stringHashKey("222123"), false},
+	}
+
+	for _, test := range failTests {
+		key, expectOk := test.key, test.expectOk
+		_, ok := sch.Get(key)
+
+		if ok != expectOk {
+			t.Errorf("ok is not as expected")
+			t.Errorf("expectOk: %+v, got: %+v", expectOk, ok)
+		}
+	}
+}
